bc5e86789abf0b0483d836102187d4e853013b4b
Munib <munibelectrical@gmail.com>
2024-05-21 17:52:16 +0500

    Add files via upload

---
+++ pro2.py
@@ -0,0 +1,26 @@
+import random
+
+top_of_range = input("Type a number ")
+print("The number to be guessed will be with-in numbers 0 to " + top_of_range)
+
+convert_top_of_range = int(top_of_range)
+rand = random.randint(0, convert_top_of_range)
+#print(rand)
+
+
+guesses = 0
+while True:
+    guesses += 1
+    guessed = input("Guess the number: ")
+    c_guessed = int(guessed)
+    if c_guessed == rand:
+        print("You got it!")
+        break
+    else:
+        if c_guessed < rand:
+            print("Your guess is below the number")
+        else:
+            print("Your guess is above the number")
+             
+    
+print("You got it in", guesses, "guesses")    
